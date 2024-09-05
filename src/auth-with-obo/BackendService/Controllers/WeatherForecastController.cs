d05c82ce701b322528c011b8db18a47986284150
Jan de Vries <jan@jan-v.nl>
2024-09-05 14:07:12 +0200

    Updated both projects and http files to do proper test along with help message

--- src/auth-with-obo/BackendService/Controllers/WeatherForecastController.cs
+++ src/auth-with-obo/BackendService/Controllers/WeatherForecastController.cs
@@ -33,8 +33,8 @@ namespace BackendService.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
+				Summary = user.DisplayName + " is " + Summaries[Random.Shared.Next(Summaries.Length)]
+			})
             .ToArray();
         }
     }
